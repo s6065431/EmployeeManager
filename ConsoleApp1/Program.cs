78f9276beb1de660f481069dd7626de85685e66d
SMALL <S6065431@live.tees.ac.uk>
2018-09-25 14:42:08 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursWorked;
+            double weeklyWage;
+            string empName;
+            string empID;
+
+            Console.WriteLine("Please Enter your name");
+            empName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Employee ID");
+            empID = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter your hours worked");
+            string input = Console.ReadLine();
+
+
+
+            hoursWorked = int.Parse(input);
+            if (hoursWorked > 40)
+            {
+                weeklyWage = 40 * 9.50;
+                hoursWorked -= 40;
+                weeklyWage += hoursWorked * 14.25;
+            }
+            else
+            {
+                weeklyWage = hoursWorked * 9.50;
+            }
+            Console.WriteLine("Your weekly wage is:  £" + weeklyWage.ToString("F"));
+            Console.ReadLine();
+
+            public boolean isValidName(empName);
+            {
+                if empName.Length
+            }
+            
+        }
+    }
+}
